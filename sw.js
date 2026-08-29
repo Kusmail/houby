d5c0b032def766e2b414582c01a3d3a1d4dec604
Kusmail <129110677+Kusmail@users.noreply.github.com>
2026-08-29 10:38:38 +0200

    v56: 16 rucne psanych lokalit pryc - overene = jen nalezy z GBIF, slozeny panel funguje i na desktopu

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
      uživatel reálně projde (nebo si je stáhne tlačítkem Offline)
    - Firebase: nikdy necachujeme, musí jít vždy na síť
    ============================================================ */
-const VER        = 'houby-v55';
+const VER        = 'houby-v56';
 const SHELL      = VER + '-shell';
 
 /* Audit 3: tohle jméno dřív obsahovalo číslo verze, takže úklid při
